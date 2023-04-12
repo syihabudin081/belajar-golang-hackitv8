6f4ba65d09056ff6223a4a0443d356cfbad4e82e
syihabudin081 <syihabupnyk@gmail.com>
2023-04-12 09:49:02 +0700

    add env variable

--- go-jwt/database/db.go
+++ go-jwt/database/db.go
@@ -1,37 +1,50 @@
 package database
 
 import (
-	"fmt"
-	"go-jwt/models"
-	"log"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+    "fmt"
+    "go-jwt/models"
+    "log"
+    "gorm.io/driver/postgres"
+    "gorm.io/gorm"
+    "github.com/joho/godotenv"
+    "os"
 )
 
 var (
-	host	 = "localhost"
-	user = "postgres"
-	password = "123"
-	dbPort = "5432"
-	dbname = "simple_api"
-	db *gorm.DB
-	err error
+    host     string
+    user     string
+    password string
+    dbPort   string
+    dbname   string
+    db       *gorm.DB
+    err      error
 )
 
+func init() {
+    // Load environment variables from .env file
+    if err := godotenv.Load(); err != nil {
+        log.Fatalf("Error loading .env file: %v", err)
+    }
 
-func StartDB(){
-
-	config := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",host,user,password,dbname,dbPort)
-	dsn := config
-	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Database connected successfully")
-	db.Debug().AutoMigrate(&models.User{},&models.Product{})
+    // Set database configuration from environment variables
+    host = os.Getenv("DB_HOST")
+    user = os.Getenv("DB_USER")
+    password = os.Getenv("DB_PASSWORD")
+    dbPort = os.Getenv("DB_PORT")
+    dbname = os.Getenv("DB_NAME")
 }
 
+func StartDB() {
+    config := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, dbPort)
+    dsn := config
+    db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println("Database connected successfully")
+    db.Debug().AutoMigrate(&models.User{}, &models.Product{})
+}
 
 func GetDB() *gorm.DB {
-	return db
+    return db
 }
--- go-jwt/main.go
+++ go-jwt/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+
 	"go-jwt/database"
 	"go-jwt/router"
+
 )
 
 func main() {
+
 	database.StartDB()
 	r := router.StartApp()
 	r.Run(":8080")
